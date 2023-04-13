f973f7300b1740a93fef280504c93c710db01f93
dryckman2 <dryckman2@huskers.unl.edu>
2023-04-13 13:16:56 -0500

    No Fill

--- chart.js
+++ chart.js
@@ -7,12 +7,12 @@ let yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
 export async function update_chart(state) {
     yValues = await get_current_overlay_data(state);
-    console.log(yValues);
     new Chart("myChart", {
         type: "line",
         data: {
             labels: xValues,
             datasets: [{
+                fill: false,
                 backgroundColor: "rgba(0,0,255,1.0)",
                 borderColor: "rgba(0,0,255,0.1)",
                 data: yValues
