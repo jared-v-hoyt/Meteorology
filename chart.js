395ad2a15c81e8b67697e8351b094944d1189a71
Jared Hoyt <jhoyt2@huskers.unl.edu>
2023-05-11 00:54:01 -0500

    Too tired to continue...

--- chart.js
+++ chart.js
@@ -1,35 +1,104 @@
-import {
-    get_current_overlay_data,
-    create_weather_data
-} from "./index.js";
+import { get_capitol_coordinates } from "./index.js";
 
-const xValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14];
+async function get_weather_data(state) {
+  const capitol = get_capitol_coordinates(state);
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=920add450c4c4cfe85f154155231404&q=${capitol.capitol}&days=3&aqi=no&alerts=no`;
+
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let data = {
+        wind: [],
+        temperature: [],
+        pressure: [],
+      };
+
+      let forecast = result.forecast.forecastday;
+
+      forecast.forEach((day) => {
+        day.hour.forEach((hour) => {
+          data.wind.push({
+            x: hour.time,
+            y: hour.wind_mph,
+          });
+          data.temperature.push({
+            x: hour.time,
+            y: hour.temp_f,
+          });
+          data.pressure.push({
+            x: hour.time,
+            y: hour.pressure_in,
+          });
+        });
+      });
+
+      return data;
+    });
+}
+
+let x_axis = [];
+
+for (let i = 0; i < 72; i++) {
+  x_axis.push(i.toString());
+}
 
 export async function update_chart(state) {
-    yValues = await create_weather_data(state);
-    new Chart("myChart", {
-        type: "line",
-        data: {
-            labels: xValues,
-            datasets: [{
-                fill: false,
-                backgroundColor: "rgba(0,0,255,1.0)",
-                borderColor: "rgba(0,0,255,0.1)",
-                data: yValues
-            }],
+  const data = await get_weather_data(state);
+  const chart = new Chart("line-chart", {
+    type: "line",
+    data: {
+      labels: x_axis,
+      datasets: [
+        {
+          label: "Wind Speed (mph)",
+          data: data.wind,
+          borderColor: "blue",
+          fill: false,
+          tension: 0.4,
         },
-        options: {
-            scales: {
-                y: {
-                    min: 0,
-                    max: 50,
-                },
-                x: {
-                    min: 0,
-                    max: 30,
-                }
-            }
-        }
-    });
+        {
+          label: "Temperature (F)",
+          data: data.temperature,
+          borderColor: "red",
+          fill: false,
+          tension: 0.4,
+        },
+        {
+          label: "Pressure (lb/in^2)",
+          data: data.pressure,
+          borderColor: "green",
+          fill: false,
+          tension: 0.4,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        title: {
+          display: true,
+        },
+      },
+      interaction: {
+        intersect: false,
+      },
+      scales: {
+        x: {
+          type: "time",
+          display: true,
+          title: {
+            display: true,
+          },
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+          },
+        },
+      },
+    },
+  });
+
+  return chart;
 }
--- index.js
+++ index.js
@@ -1,200 +1,112 @@
-import {
-	state_data
-} from "./data/state_data.js";
+import { state_outlines } from "./data/state_outlines.js";
+import { update_chart } from "./chart.js";
 
-import {
-	update_chart
-} from "./chart.js";
+let geo_json = null;
+let capitol_data = null;
+let state = null;
+let current_marker = null;
 
-let geo_json;
-let parsed_data;
-let current_state;
-let current_overlay = "wind";
-
-// Retrieve state_capitals.csv and parse the data
+// Parses state_capitols.csv and sets the capitol_data variable to the parsed object
 $.ajax({
-	url: "data/state_capitals.csv",
-	dataType: "text",
-	success: function (data) {
-		parsed_data = Papa.parse(data, {
-			header: true
-		});
-	},
+  url: "data/state_capitols.csv",
+  dataType: "text",
+  success: (data) => {
+    capitol_data = Papa.parse(data, {
+      header: true,
+    });
+  },
 });
 
-function update_data_text(data) {
-	$(".state-data").text(data);
-}
+// Returns the capitol, latitude, and longitude of the given state
+export function get_capitol_coordinates(state) {
+  let result = capitol_data.data.find((row) => {
+    return row.state === state;
+  });
 
-// Returns the latitude and longitude of the given state
-function get_capitol_coordinates(state) {
-	let result = parsed_data.data.find(function (row) {
-		return row.state === state;
-	});
+  if (result) {
+    return {
+      capitol: result.capitol,
+      latitude: result.latitude,
+      longitude: result.longitude,
+    };
+  }
 
-	if (result) {
-		return {
-			latitude: result.latitude,
-			longitude: result.longitude,
-			capital: result.capital
-		};
-	}
-
-	return null;
-}
-
-function kelvin_to_celcius(temperature_kelvin) {
-	return temperature_kelvin - 273.15;
-}
-
-// Uses the `Point Forecast` API to get the temperature data from the given state capitol
-export async function get_capitol_data(capitol) {
-	let my_headers = new Headers();
-
-	my_headers.append("Content-Type", "application/json");
-
-	let raw = JSON.stringify({
-		lat: capitol.latitude,
-		lon: capitol.longitude,
-		model: "namConus",
-		parameters: [current_overlay],
-		levels: ["surface"],
-		key: "3puqsfIl40xpSwYF3NZuFeHmhijnBNWh",
-	});
-
-	let request_options = {
-		method: "POST",
-		headers: my_headers,
-		body: raw,
-		redirect: "follow",
-	};
-
-	try {
-		const response = await fetch(
-			"https://api.windy.com/api/point-forecast/v2",
-			request_options
-		);
-		return await response.json();
-	} catch (error) {
-		console.log("error", error);
-	}
-}
-
-export async function get_current_overlay_data(state) {
-	let data = new Array();
-	create_weather_data(state);
-	let capitol_data = await get_capitol_data(get_capitol_coordinates(state));
-
-	if (current_overlay === "wind") {
-		capitol_data["wind_v-surface"].forEach((wind_record) => {
-			data.push(Math.round(wind_record));
-		});
-	} else if (current_overlay === "temp") {
-		capitol_data["temp-surface"].forEach((temperature_record) => {
-			data.push(Math.round(kelvin_to_celcius(temperature_record)));
-		});
-	} else if (current_overlay === "pressure") {
-		capitol_data["pressure-surface"].forEach((pressure_record) => {
-			data.push(Math.round(pressure_record));
-		});
-	}
-
-	return data;
-}
-
-function highlight_feature(e) {
-	var layer = e.target;
-
-	layer.setStyle({
-		weight: 3,
-		fillColor: "#666",
-		color: "#333",
-		dashArray: "",
-		fillOpacity: 0.5,
-	});
-
-	layer.bringToFront();
-}
-
-function reset_highlight(e) {
-	geo_json.resetStyle(e.target);
-}
-
-async function zoom_to_feature(e, map) {
-	map.fitBounds(e.target.getBounds());
-	current_state = e.target.feature.properties.name;
-	const data = await get_current_overlay_data(current_state);
-	update_chart(current_state);
-	$(".selected-state").text(current_state);
-	update_data_text(data);
-}
-
-function on_each_feature(feature, layer, map) {
-	layer.on({
-		mouseover: highlight_feature,
-		mouseout: reset_highlight,
-		click: function (e) {
-			zoom_to_feature(e, map);
-		},
-	});
+  return null;
 }
 
 const options = {
-	key: "xgrXMKpHDvXDH9Z2RK1d1hr5UIIyzbbL",
-	verbose: false,
-	lat: 37.8,
-	lon: -96,
-	zoom: 4,
+  // Required: API key
+  key: "xgrXMKpHDvXDH9Z2RK1d1hr5UIIyzbbL",
+
+  // Put additional console output
+  verbose: false,
+
+  // Optional: Initial state of the map
+  lat: 37.8,
+  lon: -96,
+  zoom: 4,
 };
 
 windyInit(options, (windyAPI) => {
-	const {
-		map
-	} = windyAPI;
-	const store = windyAPI.store;
+  const { map } = windyAPI;
 
-	// Listen for changes in the currently selected overlay or layer
-	store.on("overlay", (overlay) => {
-		current_overlay = overlay;
+  geo_json = L.geoJson(state_outlines, {
+    style: {
+      weight: 2,
+      opacity: 1,
+      color: "transparent",
+      dashArray: "1",
+      fillOpacity: 0.7,
+    },
+    onEachFeature: (_, layer) => {
+      layer.on({
+        mouseover: (event) => {
+          var layer = event.target;
 
-		async (data) => {
-			await get_current_overlay_data(current_state).then(
-				update_data_text(data)
-			);
-		};
-	});
+          layer.setStyle({
+            weight: 3,
+            fillColor: "#666",
+            color: "#333",
+            dashArray: "",
+            fillOpacity: 0.5,
+          });
 
-	geo_json = L.geoJson(state_data, {
-		style: {
-			weight: 2,
-			opacity: 1,
-			color: "transparent",
-			dashArray: "1",
-			fillOpacity: 0.7,
-		},
-		onEachFeature: function (feature, layer) {
-			on_each_feature(feature, layer, map);
-		},
-	}).addTo(map);
+          layer.bringToFront();
+        },
+        mouseout: (event) => {
+          geo_json.resetStyle(event.target);
+        },
+        click: async (event) => {
+          map.fitBounds(event.target.getBounds());
+          state = event.target.feature.properties.name;
+
+          if (current_marker) {
+            map.removeLayer(current_marker);
+          }
+
+          let capitol = get_capitol_coordinates(state);
+          current_marker = L.marker([capitol.latitude, capitol.longitude]).addTo(map);
+
+          current_marker.on("click", async () => {
+            $("#capitol_modal").css("display", "flex");
+            $("#modal-title").text(`${capitol.capitol}, ${state}`);
+
+            const chart = await update_chart(state);
+            chart.canvas.id = "line-chart";
+            $("#line-chart").replaceWith(chart.canvas);
+
+            $("#close-modal").on("click", () => {
+              $("#capitol_modal").css("display", "none");
+            });
+
+            window.addEventListener("click", (event) => {
+              if (event.target === capitol_modal) {
+                capitol_modal.style.display = "none";
+              }
+            });
+          });
+        },
+      });
+    },
+  }).addTo(map);
 });
-
-export async function create_weather_data(state) {
-	let capital = get_capitol_coordinates(state);
-	console.log(capital);
-	const response = await fetch("http://api.weatherapi.com/v1/forecast.json?key=920add450c4c4cfe85f154155231404&q=" + capital.capital + "&days=10&aqi=no&alerts=no");
-	const jsonData = await response.json();
-	let forecastArr = jsonData.forecast.forecastday;
-	let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-	let graphing = "avgtemp_c";
-	if (current_overlay === "wind") {
-		graphing = "maxwind_mph";
-	}
-	if (current_overlay === "temp") {
-		graphing = "avgtemp_c";
-	}
-	for (let i = 0; i < forecastArr.length; i++) {
-		arr[i] = forecastArr[i].day[graphing];
-	}
-	console.log(forecastArr);
-	return arr;
-}
